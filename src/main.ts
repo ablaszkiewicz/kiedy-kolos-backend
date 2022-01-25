453f601618b66b475df811597181a9e87c2830ec
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-01-25 12:25:54 +0100

    Change cors origin settings

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: '*' });
   await app.listen(3001);
 }
 bootstrap();
