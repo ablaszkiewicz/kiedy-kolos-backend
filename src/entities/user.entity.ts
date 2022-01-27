2f513c167940b43ea2c771660de459af68b285d5
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-01-27 20:48:26 +0100

    Cleanup

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  // @Post()
-  // async create(@Body() body): Promise<void> {
-  //   return this.usersService.create(body.email, body.password);
-  // }
+  @Post()
+  async create(@Body() body): Promise<User> {
+    return this.usersService.createUser(body.email, body.password);
+  }
 }
