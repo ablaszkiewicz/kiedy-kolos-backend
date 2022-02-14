9bc618f52d95ea1f207539bd4eba25ac935c6c7f
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-02-14 16:38:36 +0100

    Remove removed module reference

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { SubjectsModule } from './subjects/subjects.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { YearCoursesModule } from './year-courses/year-courses.module';
-import { GroupsModule } from './groups/groups.module';
 import config from '../ormconfig';
 
 @Module({
-  imports: [UsersModule, SubjectsModule, TypeOrmModule.forRoot(config), YearCoursesModule, GroupsModule],
+  imports: [UsersModule, SubjectsModule, TypeOrmModule.forRoot(config), YearCoursesModule],
   controllers: [],
 })
 export class AppModule {}
