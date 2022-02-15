a83b63f17b80ed3431ce06555717af863db75d67
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-02-15 15:25:47 +0100

    Add class validators

--- src/subjects/dto/create-subject.dto.ts
+++ src/subjects/dto/create-subject.dto.ts
@@ -1,7 +1,11 @@
+import { Length } from 'class-validator';
 import { YearCourse } from 'src/entities/yearCourse.entity';
 
-export interface CreateSubjectDTO {
+export class CreateSubjectDTO {
+  @Length(1, 150, { message: 'Nazwa przedmiotów musi mieć od 1 do 150 znaków' })
   name: string;
+
+  @Length(1, 4, { message: 'Krótka nazwa musi mieć od 1 do 4 znaków' })
   shortName: string;
   yearCourse: YearCourse;
 }
