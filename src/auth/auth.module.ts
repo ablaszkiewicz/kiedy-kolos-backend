443339b04509f0d646c2bff5c029a2130ac24455
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-02-26 11:08:55 +0100

    Fix JWT expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '5s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
