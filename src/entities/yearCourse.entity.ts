85087435a587447007dc21172f89d4b3232da804
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-02-27 13:33:36 +0100

    Modify workflow

--- src/entities/yearCourse.entity.ts
+++ src/entities/yearCourse.entity.ts
@@ -4,8 +4,6 @@ import { User } from './user.entity';
 export const MIN_START_YEAR = new Date().getFullYear() - 5;
 export const MAX_START_YEAR = new Date().getFullYear();
 
-//1
-
 @Entity()
 export class YearCourse {
   @PrimaryGeneratedColumn()
